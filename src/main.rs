ec123265e8850ec301c98739598b82946f03f966
Steve Armstrong <steve@horsefire.com>
2020-01-12 10:41:15 -0800

    Create first function with return value

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use std::env;
 use git2::Repository;
+use std::env;
 
-fn do_web() {
-    println!("do web")
-}
-
-fn do_console() {
+fn get_current_head() -> String {
     let repo = match Repository::open(".") {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
     };
     let head = repo.head().expect("Could not read head");
     let head_name = head.name().expect("Could not get head name");
-    println!("Current head: {}", head_name);
+    head_name.into()
+}
+
+fn do_web() {
+    println!("do web")
+}
+
+fn do_console() {
+    println!("Current head: {}", get_current_head());
 }
 
 fn main() {
@@ -24,6 +28,6 @@ fn main() {
     match first_arg.as_str() {
         "web" => do_web(),
         "console" => do_console(),
-        _ => panic!("Unknown command: {}", first_arg)
+        _ => panic!("Unknown command: {}", first_arg),
     }
 }
